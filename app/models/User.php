d31eb9778f9ff06d806b1129e2702413bffae52a
alexandercecile <alexander.m.cecile@gmail.com>
2024-03-16 23:37:59 -0400

    Basic work on models, DB interaction.

--- app/models/User.php
+++ app/models/User.php
@@ -18,15 +18,12 @@ class User extends \app\core\Model {
 	public function getByUsername($param_username) {
 		$raw_sql = 'SELECT * FROM user WHERE username = :username';
 		$statement = self::$_conn->prepare($raw_sql);
-		$statement->bindValue(':username', $param_username, PDO::PARAM_STR);
+		$statement->bindValue(':username', $param_username);
 		$statement->execute();
 		$statement->setFetchMode(PDO::FETCH_CLASS, 'app\models\User');
 		return $statement->fetch();
 	}
 
-
-
 }
-
 
 ?>
